88ec18273ea7d3e34aa9a6b3f183da099175b8df
Oleg Veselov <olegveselov2002@gmail.com>
2025-04-14 04:39:53 +0300

    Add files via upload

---
+++ 2_hw.py
@@ -0,0 +1,13 @@
+def task_1(f:int,g: float, h:str, i: list, k: bool)->list:
+    return f, g, h, i, k
+print(task_1(2, 2.3,'Пока', [4,5,6],False))
+
+
+def task_2(a: list):
+    return a[0:3]
+print(task_2(a = [1,2,3,5,8,13,21]))
+
+
+def task_3(b: int) -> int:
+    return b*b
+print(task_3(b=3))
